91ffa60e04383b5dcd652bbabf3e47020f7d4d11
unknown <abdofaredd@gmail.com>
2025-12-04 18:03:29 +0300

    adding setup and usestate

--- app_one/static/src/components/listView/listView.js
+++ app_one/static/src/components/listView/listView.js
@@ -1,10 +1,33 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";   
 
 export class ListViewAction extends Component {
     static template = "app_one.ListView";
+
+    setup() {
+        this.state = useState({
+            records: []
+        });
+        this.orm = useService("orm");   
+
+        this.loadRecords();
+    }
+
+    async loadRecords() {
+        try {
+            const records = await this.orm.searchRead(
+                "property",     
+                [],          
+                []
+            );
+            this.state.records = records;
+        } catch (error) {
+            console.error("Failed to load properties:", error);
+        }
+    }
 }
 
 registry.category("actions").add("app_one.action_list_view", ListViewAction);
